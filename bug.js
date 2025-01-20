fba5192658e638d1987ffa7b67c1684c1c200f8b
BugHunter-X <askzenai@gmail.com>
2025-01-20 22:24:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+The Firebase SDK might throw an error if you try to access a property of a document snapshot before the snapshot has fully loaded.  This often happens when you're working with asynchronous operations and expect data to be available immediately.
